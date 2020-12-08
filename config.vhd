76adb979d9ac36b65377ca54d8c060e0ea26e7be
Danny <44168688+igbokwedaniel@users.noreply.github.com>
2020-12-08 13:21:01 +0100

    Added config file of constants

---
+++ config.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.math_real.log2;
+use ieee.math_real.ceil;
+
+package led_controller is
+    
+    -- User configurable constants
+    constant RAM_DATA_WIDTH     : integer := 8; -- data width of ram
+    constant ROM_DATA_WIDTH     : integer := 16; -- 4 bits per column of LED Matrix
+    constant RAM_ADDRESS_WIDTH  : integer := 8; -- address width of ram
+    constant ROM_ADDRESS_WIDTH  : integer := 8; -- address width of rom
+    
+    -- Special constants (change these at your own risk, stuff might break!)
+   constant MAX_LED_COLUMN      : integer := 4; -- Number of columns in LED Matrix
+   constant MAX_RAM_ADDR        : integer := 1; -- How many letters should show up
+    
+end led_controller;
